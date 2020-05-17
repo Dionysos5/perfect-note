cfd1f1bda9a8d6d62be03962c7f9fb46c2844590
dionysos <denisbakhoum@gmail.com>
2020-05-17 21:46:55 +0000

    Checking/Unchecking items added

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+const addButton = document.getElementById('addButton');
+const list = document.getElementById('list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('checkbox')) {
+    const item = event.target;
+    toggleCheck(item);
+  }
+})
+addButton.addEventListener('click', event => { addListItem(); });
+
 function addListItem() {
-  let inputValue = document.getElementById("inputValue").value;
+  //Use an object to store data about the item
+  let inputValue = document.getElementById('inputValue').value.trim();
+
   if (inputValue !== '') {
     let item = createListItem(inputValue);
-    let list = document.getElementById("list");
+    let list = document.getElementById('list');
     list.appendChild(item);
+
   }
 
 
 }
 
 function createListItem(value) {
-  let item = document.createElement("li");
+  let item = document.createElement('li');
   let node = document.createTextNode(value);
+  let checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'checkbox';
+
+  item.appendChild(checkbox);
   item.appendChild(node);
 
   return item;
+}
+
+function toggleCheck(listItem) {
+  listItem.checked ?
+    listItem.parentElement.classList.add('checked') :
+    listItem.parentElement.classList.remove('checked');
 }
